4f69f7a3f9468956e0728bae250e7b2e5862957d
迟伤 <chishang.lcw@taobao.com>
2012-12-05 11:17:10 +0800

    提交可视化格式模型

--- demo/box/box.js
+++ demo/box/box.js
@@ -2,12 +2,23 @@
  * @author chishang.lcw
  */
 KISSY.ready(function(S){
-	var box=S.all("div");
-	 S.all(".J_Overflow").on("click",function(e){
+	var box = S.all("div");
+	 S.all(".J_Overflow").on("click", function(e){ 
 	 	
 	 	box.css({
-	 		"overflow":"hidden"
+	 		"overflow" : "hidden"
 	 	});
 	 });
+	 
+	 var versel = S.one(".J_AlignSel"),
+	 	 verBox = S.one(".J_AlignBox"),
+	 	 verEl = verBox.all(".verel");
+	 
+	 versel && versel.on("change", function(e) {
+	 	var val = versel.val();
+	 	verEl.css({
+	 		"vertical-align":val
+	 	});
+	 })
 	 
 });
