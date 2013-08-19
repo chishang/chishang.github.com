67ac054a42f2a062ffe6b00c88e93af2cf5c83b0
chishang.lcw <chishang.lcw@taobao.com>
2013-08-19 17:21:57 +0800

    事件情侣

--- demo/box/box.js
+++ demo/box/box.js
@@ -1,24 +1,24 @@
 /**
  * @author chishang.lcw
  */
-KISSY.ready(function(S){
-	var box = S.all("div");
-	 S.all(".J_Overflow").on("click", function(e){ 
-	 	
-	 	box.css({
-	 		"overflow" : "hidden"
-	 	});
-	 });
-	 
-	 var versel = S.one(".J_AlignSel"),
-	 	 verBox = S.one(".J_AlignBox"),
-	 	 verEl = verBox.all(".verel");
-	 
-	 versel && versel.on("change", function(e) {
-	 	var val = versel.val();
-	 	verEl.css({
-	 		"vertical-align":val
-	 	});
-	 })
-	 
+KISSY.ready(function (S) {
+    var box = S.all("div");
+    S.all(".J_Overflow").on("click", function (e) {
+
+        box.css({
+            "overflow": "hidden"
+        });
+    });
+
+    var versel = S.one(".J_AlignSel"),
+        verBox = S.one(".J_AlignBox"),
+        verEl = verBox.all(".verel");
+
+    versel && versel.on("change", function (e) {
+        var val = versel.val();
+        verEl.css({
+            "vertical-align": val
+        });
+    })
+
 });
--- demo/js/config.js
+++ demo/js/config.js
@@ -3,82 +3,86 @@
  */
 
 var YH_CHARSET = "utf-8",
-YH_JSROOT = "../widdget/";
+    YH_JSROOT = "../widdget/";
 KISSY.config({
-    map: [[/(.+widdget\/.+)-min.js(\?[^?]+)?$/, "$1.js$2"], [/(.+widdget\/.+)-min.css(\?[^?]+)?$/, "$1.css$2"]],
+    map: [
+        [/(.+widdget\/.+)-min.js(\?[^?]+)?$/, "$1.js$2"],
+        [/(.+widdget\/.+)-min.css(\?[^?]+)?$/, "$1.css$2"]
+    ],
     packages: [
-    //公共方法
-    {
-        name: "common",
-        path: "../js/",
-        tag: "20120324",
-        charset: "utf-8"
-    },
+        //公共方法
+        {
+            name: "common",
+            path: "../js/",
+            tag: "20120324",
+            charset: "utf-8"
+        },
     /***===============================
-	 * 公用组件
-	 =====================================*/
+     * 公用组件
+     =====================================*/
 
-    //弹窗
-    {
-        name: "win",
-        path: YH_JSROOT + "win/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-    //全选
-    {
-        name: "checkall",
-        path: YH_JSROOT + "checkall/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-    //省市区级联
-    {
-    	name:"areaselect",
-    	path:YH_JSROOT + "area/", 
-    	tag: "20120731",
-        charset: YH_CHARSET
-    },
-    //值验证
-    {
-        name: "check",
-        path: YH_JSROOT + "validation/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-    //分页
-    {
-        name: "pagenation",
-        path: YH_JSROOT + "pagenation/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-    //表单验证
-    {
-        name: "validation",
-        path: YH_JSROOT + "validation/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-    //回顶部
-    {
-        name: "top",
-        path: YH_JSROOT + "top/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-     //google 地图
-    {
-        name: "map",
-        path: YH_JSROOT + "map/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    },
-    //身份证校验
-    {
-        name: "idcardreg",
-        path: YH_JSROOT + "validation/",
-        tag: "20120324",
-        charset: YH_CHARSET
-    }]
+        //弹窗
+        {
+            name: "win",
+            path: YH_JSROOT + "win/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //全选
+        {
+            name: "checkall",
+            path: YH_JSROOT + "checkall/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //省市区级联
+        {
+            name: "areaselect",
+            path: YH_JSROOT + "area/",
+            tag: "20120731",
+            charset: YH_CHARSET
+        },
+        //值验证
+        {
+            name: "check",
+            path: YH_JSROOT + "validation/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //分页
+        {
+            name: "pagenation",
+            path: YH_JSROOT + "pagenation/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //表单验证
+        {
+            name: "validation",
+            path: YH_JSROOT + "validation/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //回顶部
+        {
+            name: "top",
+            path: YH_JSROOT + "top/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //google 地图
+        {
+            name: "map",
+            path: YH_JSROOT + "map/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        },
+        //身份证校验
+        {
+            name: "idcardreg",
+            path: YH_JSROOT + "validation/",
+            tag: "20120324",
+            charset: YH_CHARSET
+        }
+    ]
 });
